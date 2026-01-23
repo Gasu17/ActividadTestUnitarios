770fab85deebc5126c7907a8861af76c5eaf08e8
Gasu17 <sbrofati.alessandro@gmail.com>
2026-01-23 12:30:11 +0100

    Resolucion Actividad 2

--- src/test/java/es/fplumara/dam1/operaciones/testmedia.java
+++ src/test/java/es/fplumara/dam1/operaciones/testmedia.java
@@ -1,25 +1,56 @@
 package es.fplumara.dam1.operaciones;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class testmedia {
 
+
+    // PARAMETRIZADO - assertEquals
     @ParameterizedTest
     @MethodSource("notas")
-
     void testMedia (double[] notas , Double resultado){
         Double resultadoEsperado = Operaciones.media(notas);
         assertEquals (resultadoEsperado , resultado,0.0001);
     }
 
+    //assertAll
+    @Test
+    void testmediaassertall (){
+    assertAll (
+            () -> assertEquals(5.0, Operaciones.media(5,5,5), 000.1, "No es la media esperada "),
+            () -> assertEquals(6.0, Operaciones.media(7,5,6), 000.1, "No es la media esperada"),
+            () -> assertEquals(3.0, Operaciones.media(3,3,3), 000.1, "No es la media esperada")
+    );
+    }
 
+
+    // asserThrows
+    @Test
+    void mediaSinArgumento (){
+        assertThrows(IllegalArgumentException.class,
+                () -> Operaciones.media()
+
+                );
+
+    }
+    // assertThrows
+    @Test
+    void mediaConNull (){
+        assertThrows(IllegalArgumentException.class,
+                () -> Operaciones.media((double[])  null));
+
+    }
+    // ----------------------------------------------------------------------------
+
+// Datos del MethodSource
     static Stream<Arguments> notas() {
         return Stream.of(
                 Arguments.of(new double[]{5, 5, 5}, 5.0),
